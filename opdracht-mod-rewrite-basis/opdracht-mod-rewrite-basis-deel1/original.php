d020ecb8b5939c7927eb1a558c430c37242391b0
Jonas Iliaens <jonas@MacBook-Pro-van-Jonas.local>
2015-03-25 16:03:19 +0100

    Opdracht Mod Rewrite toegevoegd

---
+++ opdracht-mod-rewrite-basis/opdracht-mod-rewrite-basis-deel1/original.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht mod rewrite basis</title>
+    </head>
+    <body>
+    	<h1>Het originele bestand</h1>
+        
+    </body>
+</html>
